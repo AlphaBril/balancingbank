7982a646621207ac86b7b2559728af907532b44b
AlphaBril <florian.marie.doucet@gmail.com>
2022-03-14 19:14:38 +0100

    now the entry are correctly parsed

---
+++ api/src/controllers/upload/upload.d.ts
@@ -0,0 +1,8 @@
+export interface LCL {
+    line: string;
+    date: string;
+    libelle: string;
+    valeur: string;
+    debit: string;
+    credit: string;
+}
--- api/src/controllers/upload/upload.ts
+++ api/src/controllers/upload/upload.ts
@@ -2,6 +2,30 @@ import { internalError } from "../../shared/utils";
 import { request, response } from "../../express.d";
 import fs = require("fs");
 import { PdfData, VerbosityLevel } from "pdfdataextract";
+import { LCL } from "./upload.d";
+
+const parsePageContent = (page: string) => {
+    const result = <LCL[]>[];
+    const parsed = page.matchAll(/(\d\d\.\d\d) +(CB.+) {2}([\d ,]+)\n/gm);
+    if (parsed) {
+        for (const line of parsed) {
+            const item = <LCL>{};
+            item.line = line[0];
+            item.date = line[1];
+            item.libelle = line[2];
+            const checkCredit = item.libelle
+                .split(" ")
+                .filter((el) => el !== "");
+            if (checkCredit[checkCredit.length - 1] === ".") {
+                item.credit = line[3];
+            } else {
+                item.debit = line[3];
+            }
+            result.push(item);
+        }
+    }
+    console.log(result);
+};
 
 export const upload = async (req: request, res: response) => {
     try {
@@ -17,6 +41,7 @@ export const upload = async (req: request, res: response) => {
             }).then((data) => {
                 if (data.text) {
                     for (const page of data.text) {
+                        parsePageContent(page);
                         fs.writeFileSync("./public/test", page, {
                             encoding: "utf8",
                             flag: "a+",
